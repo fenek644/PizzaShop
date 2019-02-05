2bd9b43e97fffb4a69d95899f723a131991a9a34
fenek644 <fenek644@ya.ru>
2019-02-05 03:37:21 +0300

    Create local variable one for all pages

--- public/app.js
+++ public/app.js
@@ -1,4 +1,9 @@
 
+// window.localStorage.setItem('iks', 0);
+
 function ale() {
-    alert("Hii guys")
+    var x = parseInt(window.localStorage.getItem('iks'));
+    alert(x);
+    x = x  + 1;
+    window.localStorage.setItem('iks', x);
 }
