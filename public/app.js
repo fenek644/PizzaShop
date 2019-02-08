555056f3e8ea1545c264a2088c8eb6a8ebd64a2c
fenek644 <fenek644@ya.ru>
2019-02-08 20:04:34 +0300

    Add counter of all ordered items.

--- public/app.js
+++ public/app.js
@@ -18,4 +18,18 @@ function add_to_cart(id) {
     var x = window.localStorage.getItem(key);
     x++;
     window.localStorage.setItem(key, x);
+
+    var a = {};
+
+    var total = 0;
+
+    for (var i=0; i<localStorage.length; i++) {
+        var key = window.localStorage.key(i);
+        // a.push([key, localStorage[key]]);
+        a[key] = window.localStorage.getItem(key);
+        total += a[key]*1
+    }
+
+    // alert(total);
+
 }
